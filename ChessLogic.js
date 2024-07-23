6da87877f9fa483f94bb2930dfe9e2cd4b8a04af
Abhimanyu K Sasi <abhimanyuksasi1234@gmail.com>
2024-07-23 19:14:37 +0530

    new file:   ChessLogic.js 	new file:   Elements.js 	new file:   chess.html new file:   graphs.js 	new file:   logics.js new file:   style.css

---
+++ ChessLogic.js
@@ -0,0 +1,175 @@
+
+let MovedElement ;
+let initial_position;
+let Destination_Block;
+let final_position;
+
+
+const MoveList=[""];
+const turn= 'black';
+const display =document.getElementById('display');
+function isBlackSquare(square) {
+    if (square.length !== 2) {
+        throw new Error('Invalid square format');
+    }
+
+    var column = square.charCodeAt(0) - 97; // 'a' is 97 in ASCII
+    var row = parseInt(square[1], 10);
+
+    // Check if the sum of column and row is even or odd
+    return (column + row) % 2 === 0;
+}
+
+function Setup(){
+const board = document.querySelector('#board');
+    SQUARES.forEach((x, i) => {
+        const NewBlock = document.createElement('div');
+
+    
+            NewBlock.className =isBlackSquare(x) ?'grey_block':'black_block';
+            NewBlock.classList.add("squares");
+            
+    
+        NewBlock.id = x;
+        NewBlock.setAttribute('block_no',i);
+        if (Initial_State[i] !== '8') {
+            let NewElement = document.createElement('div');
+            NewElement.className = 'element';
+            NewElement.id = Initial_State[i];
+            const UnfPieceInfo= Initial_State[i].split('_');
+            NewElement.setAttribute('color',UnfPieceInfo[0])
+            NewElement.setAttribute('type',UnfPieceInfo[1]);
+            //part where svg for playing elements are added . 
+            NewElement.innerHTML =elementArray[UnfPieceInfo[1]]
+            NewElement.children[0].classList.add(UnfPieceInfo[0])
+            NewElement.setAttribute('draggable', true);
+            NewBlock.appendChild(NewElement);
+        }
+        board.appendChild(NewBlock);
+    });
+    
+document.querySelectorAll('.squares').forEach(Element=>{
+    Element.addEventListener("dragstart",HandleDrag);
+    Element.addEventListener("dragover",HandleDragOver);
+    Element.addEventListener('drop',HandleDrop);
+
+})
+}
+
+Setup();
+function HandleDragOver(e)
+{
+    e.preventDefault();
+
+}
+function HandleDrop(e){
+    
+    e.stopPropagation();
+    console.log(e.target.tagName);
+    
+    if(e.target.tagName=='path'){
+        Destination_Block=e.target.parentNode.parentNode.parentNode
+    }
+    else if(e.target.tagName=='svg'){
+        Destination_Block=e.target.parentNode.parentNode;
+    }
+    else if(e.target.classList.contains('element')){
+        Destination_Block=e.target.parentNode
+
+    }
+    else{
+
+        Destination_Block=e.target;}
+    
+    final_position=parseInt(Destination_Block.getAttribute('block_no'),10);
+    console.log(final_position)
+    if(Logic()){
+        perform_move();
+       
+    }
+    else{
+        alert("invalid move")
+    }
+}
+
+
+
+
+function HandleDrag(e) {
+    MovedElement= e.target;
+    initial_position=parseInt(e.target.parentNode.getAttribute('block_no'),10)
+console.log(MovedElement.id);
+console.log(initial_position);
+
+
+}
+
+
+function perform_move(){
+    if(Destination_Block.hasChildNodes()){
+        if(Destination_Block.children[0].getAttribute('color')!=MovedElement.getAttribute('color')){
+        Destination_Block.appendChild(MovedElement);
+        Destination_Block.removeChild(Destination_Block.children[0]);
+        BoardFlip()
+        moveConverter(initial_position,final_position,MoveList)
+        isCheckmate()
+        }
+        else{
+            alert('same team')
+        }}
+        
+        
+    else{
+        Destination_Block.appendChild(MovedElement);
+        BoardFlip()
+        moveConverter(initial_position,final_position,MoveList)
+        isCheckmate()
+    }
+    
+    
+}
+function BoardFlip(){
+    
+document.querySelectorAll(".squares").forEach(x=>{
+const currentBlockNo = parseInt(x.getAttribute('block_no'), 10); // Retrieve and parse the current block_no value
+const newBlockNo = 63 - currentBlockNo; // Calculate the new value
+x.setAttribute('block_no', newBlockNo); // Set the new value
+
+})}
+function Logic(){
+
+
+    if((MoveList.length%2==1 &&MovedElement.getAttribute('color')=='black')||(MoveList.length%2==0 && MovedElement.getAttribute('color')=='white')){
+        switch(MovedElement.getAttribute('type')){
+
+            case'pawn':
+            return PawnLogic(initial_position,final_position);
+            
+            case'rook':
+            return(CheckVertical(initial_position,final_position)||CheckHorizontal(initial_position,final_position))
+            case'queen':
+            return(CheckVertical(initial_position,final_position)||CheckHorizontal(initial_position,final_position)||checkDiagonal(initial_position,final_position))
+            
+            case'bishop':
+            return((checkDiagonal(initial_position,final_position)))
+            
+            case 'knight':
+                return CheckKnight(initial_position,final_position)
+            case 'king':
+                return CheckKingMove(initial_position,final_position)
+
+
+        }
+    
+    }
+}
+function moveConverter(start,end,MoveList){
+    const startRow = Math.floor(start / 8);
+    const startCol = start % 8;
+    const endRow = Math.floor(end / 8);
+    const endCol = end % 8;
+    const start_notation= String.fromCharCode(104-startCol)+""+String(startRow+1)
+    const end_notation= String.fromCharCode(104-endCol)+""+String(endRow+1)
+    MoveList.push((start_notation + " " +end_notation ))
+}
+
---
+++ logics.js
@@ -0,0 +1,153 @@
+
+        const PawnLogic =(initial_position,final_position) => {
+            const SquareList = document.querySelectorAll('.squares');
+            if (
+                (final_position === initial_position + 8 &&!Destination_Block.hasChildNodes())||((final_position ==initial_position+16) && (!SquareList.item(initial_position+8).hasChildNodes()&&!Destination_Block.hasChildNodes()))
+                ||(final_position===initial_position+7 && Destination_Block.hasChildNodes()) 
+            ||(final_position===initial_position+9 && Destination_Block.hasChildNodes())){ 
+                return true;  
+                
+              
+            }
+            return false; 
+        };
+        const RookLogic=()=>{    
+        
+        }
+        function CheckHorizontal(initialPosition,finalPosition) {
+            const [left,right] = initialPosition < finalPosition 
+                ? [initialPosition, finalPosition] 
+                : [finalPosition, initialPosition];
+            let row =null;
+            for(const x of RowList){
+                
+                if(x.includes(left)&&x.includes(right)){
+                    row = x;
+                    break;
+                }
+            }
+            if(row){
+                console.log(row);
+                const noOfBtw =right-left-1
+                
+                  //to find out the number of horizontal positions btw left and right
+                for(let i=1; i<=noOfBtw; i++){
+        
+                    if(document.querySelector(`[block_no="${row[row.indexOf(left)+i]}"]`).hasChildNodes()){
+        
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        
+        }
+        
+        function CheckVertical(initialPosition,finalPosition){
+            const [bottom, top] = initialPosition < finalPosition 
+                ? [initialPosition, finalPosition] 
+                : [finalPosition, initialPosition];
+        
+            
+            let col =null;
+            for(const x of ColList){
+                if(x.includes(bottom)&&x.includes(top)){
+                    col= x;
+                    break;
+            
+                }
+            }
+            if(col){
+                const noOfBtw=col.indexOf(top)-col.indexOf(bottom)-1
+                for(let i=1; i<=noOfBtw; i++){
+                   // console.log(document.querySelector(`[block_no="${col[col.indexOf(bottom)+i]}"]`))
+                    if(document.querySelector(`[block_no="${col[col.indexOf(bottom)+i]}"]`).hasChildNodes()){
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+        function checkDiagonal(initialPosition, finalPosition) {
+            const [bottom, top] = initialPosition < finalPosition 
+                ? [initialPosition, finalPosition] 
+                : [finalPosition, initialPosition];
+        
+            let diag = null;
+            for (const diagonal of DiagonalList) {
+                if (diagonal.includes(bottom) && diagonal.includes(top)) {
+                    diag = diagonal;
+                    break;
+                }
+            }
+        
+            if (!diag) {
+                
+                for (const antiDiagonal of AntiDiagList) {
+                    if (antiDiagonal.includes(bottom) && antiDiagonal.includes(top)) {
+                        diag = antiDiagonal;
+                        break;
+                    }
+                }
+            }
+        
+            if (diag) {
+                let [top_index,bottom_index] =(diag.indexOf(bottom)>diag.indexOf(top))?[diag.indexOf(bottom),diag.indexOf(bottom)]:[diag.indexOf(top),diag.indexOf(bottom)]
+                const noOfBtw = top_index-bottom_index-1
+                for (let i = 1; i <= noOfBtw; i++) {
+                    //console.log(document.querySelector(`[block_no="${diag[bottom_index + i]}"]`))
+                    const block = document.querySelector(`[block_no="${diag[bottom_index + i]}"]`);
+                    if (block && block.hasChildNodes()) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+        
+        function CheckKnight(initial_position, final_position){
+            const startRow =Math.floor(initial_position/8);
+            const endRow = Math.floor(final_position/8);
+            const startCol = initial_position%8;
+            const endCol = final_position%8;
+            const rowDiff = Math.abs(endRow - startRow);
+            const colDiff = Math.abs(endCol - startCol);
+        
+            // Check for valid knight move
+            return (rowDiff === 2 && colDiff === 1) || (rowDiff === 1 && colDiff === 2);
+        
+        }
+        function CheckKingMove(start, end) {
+            // Convert 1D indices to 2D coordinates
+            const startRow = Math.floor(start / 8);
+            const startCol = start % 8;
+            const endRow = Math.floor(end / 8);
+            const endCol = end % 8;
+        
+            // Calculate row and column differences
+            const rowDiff = Math.abs(endRow - startRow);
+            const colDiff = Math.abs(endCol - startCol);
+        
+            // Check for valid king move
+            return (rowDiff <= 1 && colDiff <= 1) && !(rowDiff === 0 && colDiff === 0);
+        }
+        function isCheckmate(){
+        
+        const [whiteKing,blackKing] =[document.getElementById("black_king"),document.getElementById("white_king")];
+        if(!whiteKing || !blackKing){
+            
+            document.querySelectorAll('[draggable]').forEach((x)=>{
+                x.setAttribute("draggable", false);
+            })
+            if(whiteKing===null){
+                alert("black won");}
+            else if(blackKing===null){
+        
+            alert("white won");
+            }
+        }
+        }
+     
